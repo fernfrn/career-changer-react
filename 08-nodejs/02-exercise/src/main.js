61406de81c1de59a84b4d7f0996711408f371a35
fernfrn <piyaratt.sittiyos@gmail.com>
2023-05-02 16:07:50 +0700

    commit to pull
    commit

--- 08-nodejs/02-exercise/src/main.js
+++ 08-nodejs/02-exercise/src/main.js
@@ -1,4 +1,37 @@
 import express from "express";
 const webServer = express();
+const port = 3001;
+const ipAddress = "127.0.0.1";
 
 // code down below
+webServer.get("/", (req, res) => {
+    res.send("This is / route");
+  });
+
+  webServer.get("/news", (req, res) => {
+    // res.send("This is news route");
+    const array = [];
+    const news1 = {
+        name: "news 1", 
+        detail: "detail 1" 
+    }
+    array.push(news1);
+    res.send(array);
+  });
+
+  webServer.get("/users", (req, res) => {
+    res.send("This is users route");
+  });
+
+  webServer.get("/me", (req, res) => {
+    res.send("This me route");
+  });
+
+  webServer.get("/health-check", (req, res) => {
+    res.send("This health-check users route");
+  });
+
+  webServer.listen(port, ipAddress, () => {
+    console.log(`Web Application Server is running on ${ipAddress} port ${port}`);
+    console.log(`Address: http://${ipAddress}:${port}`);
+  });
--- 08-nodejs/04-exercise/src/main.js
+++ 08-nodejs/04-exercise/src/main.js
@@ -6,7 +6,30 @@ const ipAddress = "127.0.0.1";
 const port = 3001;
 
 // code here.
-const company = [];
+const company = [
+  { 
+    companyId: "01",
+    name: "Example Company",
+    taxId: "Tax-00",
+    employees: [],
+ }
+];
+
+webServer.get("/", (request, response) => {
+  response.send("Hello world!");
+})
+
+webServer.get("/company", (request, response) => {
+  response.json(company);
+})
+
+webServer.post("/company", (request, response) => {
+  const name = response.body.name;
+  const taxId = response.body.taxId;
+  company.push({name},{taxId})
+  response.json("Created company successfully");
+})
+
 
 webServer.listen(port, ipAddress, () => {
   console.log(`Web Application Server is running on ${ipAddress} port ${port}`);
